fe1a9a08864f6f3b21a7b1a8c264b4a1a6e8bbcd
Antoni Zajko <antoni.zajko.1@gmail.com>
2022-10-06 00:55:57 +0200

    Remove unwanted files

--- pipelines/categorical/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["OneHotColumnEncoder", "CategoryEmbedder"]
-
-from pipelines.categorical.one_hot import *
-from pipelines.categorical.category_embedder import *
--- pipelines/numerical/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["AgeDataPreprocessor", "ToGaussianTransformer"]
-
-from pipelines.numerical.age_data_preprocessor import *
-from pipelines.numerical.gaussian_like import *
